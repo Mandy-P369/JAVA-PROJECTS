01c3fa1355b1e5afae96fb2307045c6c1d59f815
Mandy-P369 <aayushpateriya762@gmail.com>
2025-07-10 12:44:29 +0530

    Add files via upload

---
+++ WMS.java
@@ -0,0 +1,16 @@
+
+package wms;
+
+/**
+ *
+ * @author Ayush
+ */
+public class WMS {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
